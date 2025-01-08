f47a2065be40e4605608ee9babe6cc5dd5fd1770
Lior Medvinsky <liormedvinsky@gmail.com>
2025-01-08 11:13:32 -0500

    set frame to screen size

--- src/CaptureRectangle.java
+++ src/CaptureRectangle.java
@@ -2,6 +2,8 @@ import java.awt.image.BufferedImage;
 import javax.swing.JFrame;
 import java.awt.Rectangle;
 import java.awt.Robot;
+import java.awt.Dimension;
+import java.awt.Toolkit;
 
 // a class with methods involved in selecting and capturing the area to screenshot and returning said screenshot
 // unfinished
@@ -14,8 +16,12 @@ public class CaptureRectangle {
     public static Rectangle captureRectangle(){
         JFrame frame = new JFrame();
 
+        // size of the screen
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        
+
         // set the frame to be the whole screen
-        frame.setSize(500, 600); //todo: set size max
+        frame.setSize(screenSize.width, screenSize.height);
         frame.setLocation(0,0); // sets at top left corner        
 
         // set the frame to be opaque

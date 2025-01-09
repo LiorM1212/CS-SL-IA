92d841ac419868d591df77cf7c9b63226a065fa7
Lior Medvinsky <liormedvinsky@gmail.com>
2025-01-09 09:10:59 -0500

    add confirm button to frame

--- src/CaptureRectangle.java
+++ src/CaptureRectangle.java
@@ -4,10 +4,19 @@ import java.awt.Rectangle;
 import java.awt.Robot;
 import java.awt.Dimension;
 import java.awt.Toolkit;
+import java.awt.Button;
+import java.awt.Color;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 // a class with methods involved in selecting and capturing the area to screenshot and returning said screenshot
 // unfinished
 public class CaptureRectangle {
+
+    // for the button in the captureRectangle method
+    // needs to be a global for captureRectangle to work properly
+    static boolean clicked = false;
+
     /*
      * @return the rectangle of the user selected area 
      * 
@@ -15,14 +24,6 @@ public class CaptureRectangle {
      */
     public static Rectangle captureRectangle(){
         
-        openFrame();
-
-        // return selected rectangle 
-        return null;
-    }
-
-    // a method that opens a frame in which the user will interact
-    private static void openFrame(){
         JFrame frame = new JFrame();
 
         // size of the screen
@@ -32,14 +33,42 @@ public class CaptureRectangle {
         frame.setSize(screenSize.width, screenSize.height);
         frame.setLocation(0,0); // sets at top left corner        
 
-        // set the frame to be opaque
         frame.setUndecorated(true);
+
+        // set the frame to be opaque
         float opacity = .3f;
         frame.setOpacity(opacity);
 
-        // add an adjustable rectangle on the screen for the user to adjust
-        frame.add(new AdjustableRectangle());
+        // create confirmation button
+        Button confirmButton = new Button("OK");
+        confirmButton.setBounds(screenSize.width/2,0,50,50);
+        confirmButton.setBackground(Color.GREEN);
+
+        // create selection rectangle
+        AdjustableRectangle adjustableRectangle = new AdjustableRectangle();
+
+        
+
+        // when button pressed return rectangle
+        confirmButton.addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                clicked = true;
+                frame.removeAll();
+                frame.setVisible(false);
+            }
+        });
+
+        frame.add(confirmButton);
+        frame.add(adjustableRectangle);
         frame.setVisible(true);
+
+        // wait for the confirmation to be clicked
+        // a method call in the while is neccessary for it to work
+        // some methods seem to work others don't
+        while(!clicked){System.out.print("");} // ugly code
+        clicked = false;
+        return adjustableRectangle.getRectangle();
     }
 
     /* @param bounds rectangle of screen to take screenshot of
